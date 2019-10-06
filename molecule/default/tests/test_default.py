ae579b12bff5cff6d97130dbabc0bd9fd4eb34f9
Christoffer Viken <christoffer@viken.me>
2019-10-06 11:25:16 +0200

    Add EL support and bump to v0.7.0
     * Add some rudamentary testing
     * Add EL testing (CentOS)
     * Skip tests that aren't used

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,37 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_thanos_executable(host):
+    f = host.file('/opt/thanos/current/thanos')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+
+def test_thanos_bucket_config(host):
+    f = host.file('/etc/thanos/bucket_config.yaml')
+
+    assert f.exists
+
+
+def test_sidecar(host):
+    f = host.file('/etc/systemd/system/thanos-sidecar.service')
+
+    assert f.exists
+
+
+def test_query(host):
+    f = host.file('/etc/systemd/system/thanos-query.service')
+
+    assert f.exists
+
+
+def test_store(host):
+    f = host.file('/etc/systemd/system/thanos-store.service')
+
+    assert f.exists
+
+
+def test_compactor(host):
+    f = host.file('/etc/systemd/system/thanos-compactor.service')
+
+    assert f.exists
